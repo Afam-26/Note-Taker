1b230ac0b47cf769bb61e656eafe5afb750dac19
Afam <afam262@gmail.com>
2020-12-23 01:22:12 -0600

    move server file

--- Develop/server.js
+++ Develop/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 app.get("/", function(req, res) {
   // res.send("Proof of concept of storing data");
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", function(req, res) {
