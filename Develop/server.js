b0fbb7d47a3d08730875f6a111bbb0d6f0951f76
Afam <afam262@gmail.com>
2020-12-28 23:21:38 -0600

    add access control allow origin to server file and readme contents

--- Develop/server.js
+++ Develop/server.js
@@ -15,6 +15,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
+
 
 
 // Routes
@@ -33,7 +38,7 @@ app.get("/notes", function(req, res) {
 });
 
 app.get("/api/notes", function(req, res) {
-
+  
   res.json(dbJSON);
   
 });
@@ -49,11 +54,12 @@ app.post("/api/notes", function(req, res) {
 
   // Push note to dbJSON array - saves data in memory
   dbJSON.push(note);
+  
 
   // Saves data to file by persisting in memory variable dbJSON to db.json file.
   // This is needed because when we turn off server we loose all memory data like pbJSON variable.
   // Saving to file allows us to read previous notes (before server was shutdown) from file.
-  fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(dbJSON), (err) => {
+  fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(dbJSON), (err) => {
     if (err) {
       return res.json({error: "Error writing to file"});
     }
@@ -67,7 +73,7 @@ app.delete('/api/notes/:id', function (req, res) {
 })
 
 app.get("*", function(req, res) {
-  // res.send("Sending you the homepage");
+  //
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
